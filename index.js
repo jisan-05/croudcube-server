4995dfe78da029fa3368d2427a84512769bb6ffe
Tanvir <zishantanvir05@gmail.com>
2025-02-28 11:23:03 +0600

    deleted method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -47,6 +47,14 @@ async function run() {
             const result = await campaignCollection.find(query).toArray()
             res.send(result)
         });
+
+        app.delete('/campaign/:id', async (req,res) => {
+            const id = req.params.id;
+            console.log("Please delete a id", id)
+            const query = {_id:new ObjectId(id)}
+            const result = await campaignCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
